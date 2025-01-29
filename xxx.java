1bcde9a284a37022b4ac05f524cdebd59d415448
redxcyber <mrdark9628@gmail.com>
2025-01-29 22:56:50 +0530

    push code from mobile

--- xxx.java
+++ xxx.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class xxx{
 	public static void main(String[] args){
 		System.out.println("hello");
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Enter a number : ");
+    int num = sc.nextInt();
+    System.out.println("your num is "+num);
+
 	}
 }
