ba0d712dcea4d08b45ce6367df28bfae02b551bd
redxcyber <mrdark9628@gmail.com>
2025-01-30 21:48:25 +0530

    push from laptop

---
+++ PrintPrime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintPrime {
+    public static void main(String[] args) {
+        for(int i=100; i<=200; i++){
+            boolean flag = true;
+            for(int j=2; j<Math.sqrt(i); j++){
+                if(i%j==0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
