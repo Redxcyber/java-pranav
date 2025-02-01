c658086ea223dc09b96e56c7613fed724b1179db
redxcyber <mrdark9628@gmail.com>
2025-02-01 10:49:40 +0530

    kl

--- college/SwapVariable.java
+++ college/SwapVariable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SwapNumber {
+public class SwapVariable {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter A : ");
