6e8d482b29eb4b5ab2c1c90703f2fa9e89f0c697
redxcyber <mrdark9628@gmail.com>
2025-02-01 08:29:31 +0530

    push from laptop

---
+++ college/Factorial.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Factorial{
+
+    //non-recursive factorial function
+    static long nonRecursiveFactorial(int n){
+        long fact=1;
+        for(int i=n; i>1; i--){
+            fact = fact * i;
+        }
+        return fact;
+    }
+    
+    //recursive factorial function
+    static long recursiveFactorial(int n){
+        if(n==0||n==1){
+            return 1;
+        }else{
+            return n*recursiveFactorial(n-1);
+        }
+    }
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        //input number 
+        System.out.print("Enter number : ");
+        int n = input.nextInt();
+
+        //non-recursive factorial
+        System.out.println("Factorial : "+nonRecursiveFactorial(n));
+
+        //recursive factorial
+        System.out.println("Factorial : "+recursiveFactorial(n));
+
+
+    }
+}
