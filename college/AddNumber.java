05c67b42e27b4aee01e49a99784435b54194be6d
redxcyber <mrdark9628@gmail.com>
2025-02-01 10:45:09 +0530

    kl

--- college/AddNumber.java
+++ college/AddNumber.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class AddNumber {
     public static void main(String[] args) {
-        try(Scanner input = new Scanner(System.in)) {
-            System.out.print("Enter two numbers : ");
-            int a = input.nextInt();
-            int b = input.nextInt();
-            int result = a+b;
-            System.out.println("Sum : "+result);
-        }
+        Scanner input = new Scanner(System.in))
+        System.out.print("Enter two numbers : ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int result = a+b;
+        System.out.println("Sum : "+result);
 
     }
 }
---
+++ college/SwapNumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter A : ");
+        int a = input.nextInt();
+        System.out.print("Enter B : ");
+        int b = input.nextInt();
+        input.close();
+
+        System.out.println("Before swapping -> A : "+a+", B : "+b);
+
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping -> A : "+a+", B : "+b);
+
+    }
+}
