630cd3c867abbf1b5546e49d06ec02252e23bb92
redxcyber <mrdark9628@gmail.com>
2025-02-01 10:46:19 +0530

    kl

--- college/AddNumber.java
+++ college/AddNumber.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class AddNumber {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in))
+        Scanner input = new Scanner(System.in);
         System.out.print("Enter two numbers : ");
         int a = input.nextInt();
         int b = input.nextInt();
